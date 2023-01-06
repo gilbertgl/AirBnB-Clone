a6af36374a6c4c8daee12cf9b44a68510f824e4b
Gilbero M <gilberto.mamedes13@gmail.com>
2023-01-05 20:58:43 -0400

    map and list sync

--- screens/searchResultsMap/index.js
+++ screens/searchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, Text, FlatList, useWindowDimensions } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { PROVIDER_GOOGLE } from "react-native-maps/lib/ProviderConstants";
@@ -12,10 +12,36 @@ import places from "../../assets/data/feed";
 const SearchResultsMap = (props) => {
   const [selectedPlaceId, setSelectedPlaceId] = useState(null);
   const width = useWindowDimensions().width;
+  const flatList = useRef();
+  const map = useRef();
+  const viewConfig = useRef({ itemVisiblePercentThreshold: 70 });
+  const onViewChanged = useRef(({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id);
+    }
+  });
+
+  useEffect(() => {
+    if (selectedPlaceId || selectedPlaceId) {
+      const index = places.findIndex((place) => place.id === selectedPlaceId);
+      flatList.current.scrollToIndex({ index });
+
+      const selectedPlace = places[index];
+      const region = {
+        latitude: selectedPlace.coordinate.latitude,
+        longitude: selectedPlace.coordinate.longitude,
+        latitudeDelta: 0.8,
+        longitudeDelta: 0.8,
+      };
+      map.current.animateToRegion(region);
+    }
+  }, [selectedPlaceId]);
 
   return (
     <View style={styles.container}>
       <MapView
+        ref={map}
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={{
@@ -36,6 +62,7 @@ const SearchResultsMap = (props) => {
       </MapView>
       <View style={styles.corousel}>
         <FlatList
+          ref={flatList}
           data={places}
           renderItem={({ item }) => <PostCarouselItem post={item} />}
           horizontal
@@ -43,6 +70,8 @@ const SearchResultsMap = (props) => {
           snapToInterval={width - 30}
           snapToAlignment={"center"}
           decelerationRate={"fast"}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
