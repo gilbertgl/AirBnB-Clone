f7ecae0d505035d8a186a44fe56f285ffa9c243a
Gilbero M <gilberto.mamedes13@gmail.com>
2023-01-06 13:03:50 -0400

    amplify init

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from "react";
 import { StatusBar } from "react-native";
 import Router from './navigation/router';
 
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+Amplify.configure(awsconfig)
+
 export default function App() {
   return (
     <>
