e791db4786a5e3710f0434ef8691430a1707e299
Gilbero M <gilberto.mamedes13@gmail.com>
2023-01-11 13:47:07 -0400

    details screen working

--- screens/accommodationsDetails/index.js
+++ screens/accommodationsDetails/index.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { API, graphqlOperation } from "aws-amplify";
+import { getPost } from "../../src/graphql/queries";
 
 import styles from "./styles";
 import DetailedPost from "../../components/detailedPost";
-import places from "../../assets/data/feed";
-
 
 const AcomodationDetailsScreen = (props) => {
-    const route = useRoute();
-    const post = places.find(place => place.id == route.params.postId);
+  const [post, setPost] = useState([]);
+  const route = useRoute();
+
+  //console.log(post);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const postResullts = await API.graphql(
+          graphqlOperation(getPost, {
+              id: route.params.postId,
+          })
+        );
+        setPost(postResullts.data.getPost);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
 
   return (
     <View style={styles.container}>
